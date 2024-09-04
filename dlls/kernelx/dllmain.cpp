eb60a081868a5adb286acaf4dc3ab0b9cbff3e07
Tyler Jaacks <tylerjaacks@gmail.com>
2024-09-03 23:23:01 -0500

    Added XMemAlloc and TpSetPoolAffinityMask psuedocode.

--- dlls/kernelx/dllmain.cpp
+++ dlls/kernelx/dllmain.cpp
@@ -51,6 +51,27 @@ NTSYSAPI VOID RtlCaptureContext_X(PCONTEXT ContextRecord)
 // dwAttributes - allocated memory block attributes.
 //
 // returns a pointer allocated memory block.
+
+/*
+ULONGLONG __fastcall XMemAlloc(ULONG64 a1, __int64 a2)
+{
+	ULONGLONG v4; // rbx
+	_EVENT_TRACE_HEADER TraceHeader; // [rsp+20h] [rbp-48h] BYREF
+	__int64 v7; // [rsp+50h] [rbp-18h]
+
+	v4 = off_18002B1B8();
+	if (MEMORY[0x7FFE0390] && v4)
+	{
+		*(&TraceHeader.GuidPtr + 1) = v4;
+		TraceHeader.Class.Version = 3104;
+		TraceHeader.ProcessorTime = a1;
+		v7 = a2;
+		NtTraceEvent(MEMORY[0x7FFE0390], 0x10402u, 0x18u, &TraceHeader);
+	}
+	return v4;
+}
+*/
+
 PVOID XMemAlloc_X(SIZE_T dwSize, ULONGLONG dwAttributes)
 {
 	return VirtualAlloc(nullptr, dwSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
