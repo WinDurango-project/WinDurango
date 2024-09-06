b22d2403261a3d433247a998c9c7875de5cf4bed
Serenity <34622204+oSerenity@users.noreply.github.com>
2024-09-06 01:47:15 -0400

    Bad_alloc Fix

--- dlls/kernelx/dllmain.cpp
+++ dlls/kernelx/dllmain.cpp
@@ -1,53 +1,74 @@
-// ReSharper disable CppInconsistentNaming
-// ReSharper disable CppParameterMayBeConst
-// ReSharper disable CppDefaultCaseNotHandledInSwitchStatement
-// ReSharper disable CppClangTidyHicppMultiwayPathsCovered
-// ReSharper disable CppClangTidyClangDiagnosticInconsistentDllimport
 #include "pch.h"
 #include "kernelx.h"
-
-
+#include <cstdlib>
 
 ANSI_STRING* DestinationString;
 SYSTEM_BASIC_INFORMATION* System_Info;
 
 HINSTANCE hModule;
 
-
-
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD forwardReason, LPVOID lpvReserved)
 {
-    BOOL v4;
-	switch (forwardReason)
-	{
-	case DLL_PROCESS_ATTACH:
+    BOOL result = TRUE; // Default to TRUE for all cases
+    switch (forwardReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Allocate and initialize the global pointers
+        DestinationString = (ANSI_STRING*)malloc(sizeof(ANSI_STRING));
+        if (!DestinationString) {
+            return FALSE; // Fail if memory allocation fails
+        }
+        System_Info = (SYSTEM_BASIC_INFORMATION*)malloc(sizeof(SYSTEM_BASIC_INFORMATION));
+        if (!System_Info) {
+            free(DestinationString); // Clean up previously allocated memory
+            return FALSE;
+        }
 
-        if (NtQuerySystemInformation(SystemBasicInformation, &System_Info, 0x40u, 0LL) >= 0)
-        {
-            v4 = true;
-            if (RtlUnicodeStringToAnsiString(DestinationString OUT, &NtCurrentTeb()->ProcessEnvironmentBlock->ProcessParameters->CommandLine, true) < 0)
-            {
-                DestinationString->Length = 0LL;
-                DestinationString->Buffer = 0LL;
-            }
-            //Probably have to change this to SetUnhandledExceptionFilter
-            SetUnhandledExceptionFilter(UnhandledExceptionFilter);
+        if (NtQuerySystemInformation(SystemBasicInformation, System_Info, sizeof(SYSTEM_BASIC_INFORMATION), NULL) < 0) {
+            free(DestinationString); // Clean up
+            free(System_Info);
+            return FALSE; // Fail if system info can't be obtained
         }
-        else
-        {
-            v4 = 0;
+
+        DestinationString->Buffer = (char*)malloc(1024); // Allocate buffer for ANSI string
+        if (!DestinationString->Buffer) {
+            free(DestinationString); // Clean up
+            free(System_Info);
+            return FALSE;
         }
+        DestinationString->Length = 0;
+        DestinationString->MaximumLength = 1024;
+
+        if (RtlUnicodeStringToAnsiString(DestinationString, &NtCurrentTeb()->ProcessEnvironmentBlock->ProcessParameters->CommandLine, FALSE) < 0) {
+            free(DestinationString->Buffer); // Clean up
+            free(DestinationString);
+            free(System_Info);
+            return FALSE;
+        }
+
+        // Set exception handler
+        SetUnhandledExceptionFilter(UnhandledExceptionFilter);
         hModule = hInstance;
         sub_18001BB8C();
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-        v4 = 1;
-        sub_18001BCA0(hInstance, forwardReason, lpvReserved);
-		break;
-	}
+        break;
 
-	return TRUE;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        // Perform cleanup on process detach
+        if (forwardReason == DLL_PROCESS_DETACH) {
+            if (DestinationString) {
+                if (DestinationString->Buffer)
+                    free(DestinationString->Buffer);
+                free(DestinationString);
+            }
+            if (System_Info) {
+                free(System_Info);
+            }
+        }
+        result = sub_18001BCA0(hInstance, forwardReason, lpvReserved);
+        break;
+    }
+
+    return result;
 }
-
-
