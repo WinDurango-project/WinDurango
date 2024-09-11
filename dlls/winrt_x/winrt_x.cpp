40533ce1135ac4ada762917cf9478f5ffdcc39ef
Tyler Jaacks <tylerjaacks@gmail.com>
2024-09-10 20:47:23 -0500

    Fixed build errors.

--- dlls/winrt_x/winrt_x.cpp
+++ dlls/winrt_x/winrt_x.cpp
@@ -3,12 +3,14 @@
 
 HRESULT GetActivationFactory_X(HSTRING classId, IActivationFactory** factory)
 {
-	switch (classId) 
+	/*
+	switch (classId)
 	{
 	case "Windows.Xbox.Achievements.AchievementNotifier":
 	default:
 		break;
 	}
+	*/
 
 	return S_OK;
 }
