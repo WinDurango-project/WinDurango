9a07d7f49b232e5faabbf0c0bc1adfe90df9d380
Tyler Jaacks <tylerjaacks@gmail.com>
2024-09-12 20:23:47 -0500

    Added some basic D3D11 support.

--- dlls/d3d11_x/d3d11_x.cpp
+++ dlls/d3d11_x/d3d11_x.cpp
@@ -1,2 +1,63 @@
 // ReSharper disable CppInconsistentNaming
+// ReSharper disable CppClangTidyClangDiagnosticUnusedFunction
 #include "pch.h"
+
+void D3DQuerySEQCounters_X()
+{
+
+}
+
+void D3DUploadCustomMicrocode_X()
+{
+
+}
+
+void D3D11XCreateDeviceXAndSwapChain1_X()
+{
+
+}
+
+void D3DAllocateGraphicsMemory_X()
+{
+
+}
+
+void D3DConfigureVirtualMemory_X()
+{
+
+}
+
+void D3DFlushEntireCpuCache_X()
+{
+
+}
+
+void D3DFreeGraphicsMemory_X()
+{
+
+}
+
+void D3DMapEsramMemory_X()
+{
+
+}
+
+void DXGIXGetFrameStatistics_X()
+{
+
+}
+
+void DXGIXPresentArray_X()
+{
+
+}
+
+void DXGIXSetFrameNotification_X()
+{
+
+}
+
+void DXGIXSetVLineNotification_X()
+{
+
+}
