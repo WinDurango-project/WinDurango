311e76a0fdb86fb877ff48b806f21eae67ca0418
Rodrigo Todescatto <90097545+TechSasuke@users.noreply.github.com>
2024-09-15 14:20:27 -0300

    Added exports.

--- dlls/xg_x/xg_x.cpp
+++ dlls/xg_x/xg_x.cpp
@@ -5,18 +5,76 @@
 #include "framework.h"
 #include "xg_x.h"
 
+XGX_API	void XGComputeBufferLayout_X()
+{
+
+}
+
+XGX_API	void XGComputeOptimalDepthStencilTileModes_X()
+{
+
+}
+
+XGX_API	void XGComputeOptimalTileMode_X()
+{
+
+}
+
+XGX_API	void XGComputeTexture1DLayout_X()
+{
+
+}
+
+XGX_API	void XGComputeTexture2DLayout_X()
+{
+
+}
+
+XGX_API	void XGComputeTexture3DLayout_X()
+{
+
+}
+
+XGX_API	void XGComputeTileShape_X()
+{
+
+}
+
+XGX_API	void XGCreateTexture1DComputer_X()
+{
+
+}
+
+XGX_API	void XGCreateTexture2DComputer_X()
+{
+
+}
+
+XGX_API	void XGCreateTexture3DComputer_X()
+{
+
+}
+
+XGX_API	void XGValidateJpegFile_X()
+{
+
+}
+
+
 
 // This is an example of an exported variable
-XGX_API int nxgx=0;
+	XGX_API int nxgx = 0;
+
 
 // This is an example of an exported function.
-XGX_API int fnxgx(void)
-{
-    return 0;
-}
+	XGX_API int fnxgx(void)
+	{
+	    return 0;
+	}
+
 
 // This is the constructor of a class that has been exported.
-Cxgx::Cxgx()
-{
-    return;
-}
+	Cxgx::Cxgx()
+	{
+	    return;
+	}
