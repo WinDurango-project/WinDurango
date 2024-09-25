882dd668d8a4c86d4030d4a1c99ef35f6aa6072d
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2024-09-25 19:44:56 -0300

    Delete dlls/pch.cpp

--- dlls/pch.cpp
+++
@@ -1,5 +0,0 @@
-// pch.cpp: arquivo de origem correspondente ao cabeçalho pré-compilado
-
-#include "pch.h"
-
-// Quando você estiver usando cabeçalhos pré-compilados, esse arquivo de origem será necessário para que a compilação seja bem-sucedida.
