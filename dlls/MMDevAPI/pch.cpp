fd24b0d45f19895822c500e9dd93d45251180b6e
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2024-09-25 19:48:08 -0300

    Added MMDevAPI.

---
+++ dlls/MMDevAPI/pch.cpp
@@ -0,0 +1,5 @@
+// pch.cpp: arquivo de origem correspondente ao cabeçalho pré-compilado
+
+#include "pch.h"
+
+// Quando você estiver usando cabeçalhos pré-compilados, esse arquivo de origem será necessário para que a compilação seja bem-sucedida.
