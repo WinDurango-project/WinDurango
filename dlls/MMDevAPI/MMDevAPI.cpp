66421c960a42428ab55deabfa7de593c69545c76
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2024-09-26 02:25:56 -0300

    Added MMDevAPI.dll (Audio Related). (#50)
    * Added MMDevAPI.
    
    * Create MMDevAPI
    
    * Delete dlls/MMDevAPI
    
    * Create ignore
    
    * Delete dlls/MMDevAPI/ignore
    
    * Added MMDevAPI.
    
    * Delete dlls/Exports.def, Delete dlls/MMDevAPI.cpp
    
    * Delete dlls/MMDevAPI.cpp
    
    * Delete dlls/pch.h
    
    * Delete dlls/pch.cpp
    
    * Delete dlls/dllmain.cpp
    
    * Delete dlls/MMDevAPI.vcxproj
    
    * Delete dlls/MMDevAPI.sln
    
    * Delete dlls/framework.h
    
    * Delete dlls/MMDevAPI.vcxproj.filters
    
    * Delete dlls/MMDevAPI.vcxproj.user
    
    * Added MMDevAPI.
    
    * Create ignore
    
    * Added MMDevAPI.
    
    * Delete dlls/MMDevAPI/ignore

---
+++ dlls/MMDevAPI/MMDevAPI.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "framework.h"
+
+void ActivateAudioInterfaceAsync_X()
+{
+
+}
+
+void DllCanUnloadNow_X()
+{
+
+}
+
+void DllGetClassObject_X()
+{
+
+}
+
+void RefreshWasapiDeviceList_X()
+{
+
+}
+
+void SetWasapiThreadAffinityMask_X()
+{
+
+}
