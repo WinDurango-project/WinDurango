ebaf02ea810971494a7ba65c5648a5838411869e
Bagietka <bagietabusiness@gmail.com>
2024-09-29 22:28:10 +0200

    KernelX, CONSOLE_TYPE enum

--- dlls/kernelx/kernelx.cpp
+++ dlls/kernelx/kernelx.cpp
@@ -258,7 +258,7 @@ void GetSystemOSVersion_X(uint8_t* buffer) {
 
 
 __int64 GetConsoleType_X() {
-    return 4;
+    return CONSOLE_TYPE::CONSOLE_TYPE_XBOX_ONE_X_DEVKIT;
 }
 
 uint32_t dword_180021AA0[16];
