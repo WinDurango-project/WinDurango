363caccb08428def98d6dd6fc04cc46f8573ba7c
Bagietka <bagietabusiness@gmail.com>
2024-10-03 22:01:01 +0200

    AcpHal changes
    Methods types set

--- dlls/AcpHal/AcpHal.cpp
+++ dlls/AcpHal/AcpHal.cpp
@@ -1,41 +1,48 @@
 #include "pch.h"
+#include "AcpHal.h"
+#include <stdlib.h>
 
-__int64 AcpHalAllocateShapeContexts_X(__int64 a1) {
+struct IAcpHal;
+
+HRESULT AcpHalAllocateShapeContexts_X(__int64 a1) {
     return 0;
 }
 
-__int64 AcpHalCreate_X(unsigned __int64* a1) {
+HRESULT AcpHalCreate_X(IAcpHal* a1) {
     return 0;
 }
 
-__int64 AcpHalReleaseShapeContexts_X() {
+HRESULT AcpHalReleaseShapeContexts_X() {
     return 0;
 }
 
-__int64 ApuAlloc_X(int a1, int a2, int a3, int a4, int a5) {
+HRESULT ApuAlloc_X(void* ptr, void* a2, size_t size, UINT32 alignment) {
+    ptr = malloc(size);
     return 0;
 }
 
-__int64 ApuCreateHeap_X(int a1, int a2) {
+HRESULT ApuCreateHeap_X(size_t initialSize, size_t maximumSize) {
+    // Don't think we need that as this is for chaning memory pool iirc...
     return 0;
 }
 
-__int64 ApuFree_X(__int64 a1) {
+HRESULT ApuFree_X(void* ptr) {
+    free(ptr);
     return 0;
 }
 
-__int64 ApuHeapGetState_X(__int64 a1, int a2) {
+HRESULT ApuHeapGetState_X(ApuHeapState* a1, int type) {
     return 0;
 }
 
-__int64 ApuIsVirtualAddressValid_X(unsigned __int64 a1, unsigned int a2) {
+HRESULT ApuIsVirtualAddressValid_X(unsigned __int64 a1, unsigned int a2) {
     return 0;
 }
 
-__int64 ApuMapApuAddress_X(unsigned int a1) {
+HRESULT ApuMapApuAddress_X(unsigned int a1) {
     return 0;
 }
 
-__int64 ApuMapVirtualAddress_X(unsigned __int64 a1) {
+HRESULT ApuMapVirtualAddress_X(unsigned __int64 a1) {
     return 0;
 }
