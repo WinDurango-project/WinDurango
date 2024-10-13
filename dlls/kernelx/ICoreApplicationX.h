678ca0e912b660c1811f7acf9b1a748f840d8fa4
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2024-10-13 15:10:32 -0300

    Added some ICoreApplication basic stuff.

---
+++ dlls/kernelx/ICoreApplicationX.h
@@ -0,0 +1,39 @@
+// ReSharper disable CppPolymorphicClassWithNonVirtualPublicDestructor
+// ReSharper disable CppInconsistentNaming
+// ReSharper disable CppClangTidyClangDiagnosticNonVirtualDtor
+// ReSharper disable IdentifierTypo
+// ReSharper disable CppClangTidyClangDiagnosticHeaderHygiene
+#include <winrt/windows.foundation.collections.h>
+#include <winrt/Windows.ApplicationModel.h>
+#include <windows.ui.core.h>
+#include <inspectable.h>
+
+
+using namespace ABI::Windows::ApplicationModel::Activation;
+using namespace ABI::Windows::ApplicationModel;
+using namespace ABI::Windows::Foundation;
+using namespace ABI::Windows::ApplicationModel::Core;
+using namespace ABI::Windows::System;
+
+class ICoreApplicationX : public IInspectable
+{
+public:
+	virtual INT32 _abi_add_Resuming(__FIEventHandler_1_IInspectable* handler, EventRegistrationToken* token) = 0;
+	virtual INT32 _abi_remove_Resuming(EventRegistrationToken token) = 0;
+	virtual INT32 _abi_add_Suspending(__FIEventHandler_1_Windows__CApplicationModel__CSuspendingEventArgs* handler, EventRegistrationToken* token) = 0;
+	virtual INT32 _abi_remove_Suspending(EventRegistrationToken token) = 0;
+	virtual INT32 _abi_get_ResourceAvailability() = 0;
+	virtual INT32 _abi_add_ResourceAvailabilityChanged(winrt::Windows::Foundation::EventHandler<IInspectable>* handler, EventRegistrationToken* token) = 0;
+	virtual INT32 _abi_remove_ResourceAvailabilityChanged(EventRegistrationToken token) = 0;
+
+	virtual INT32 _abi_RunWithActivationFactories(ABI::Windows::Foundation::IGetActivationFactory* activationFactoryCallback) = 0;
+	virtual INT32 _abi_GetCurrentView(ABI::Windows::ApplicationModel::Core::ICoreApplicationView** value) = 0;
+	virtual INT32 _abi_Run(ABI::Windows::ApplicationModel::Core::IFrameworkViewSource* viewSource) = 0;
+	virtual INT32 Run(ABI::Windows::ApplicationModel::Core::IFrameworkViewSource* viewSource) = 0;
+	virtual INT32 _abi_get_Id(HSTRING* value) = 0;
+	virtual INT32 _abi_get_Properties(ABI::Windows::Foundation::Collections::IPropertySet** value) = 0;
+
+	virtual HRESULT QueryInterface(const IID& riid, void** ppvObject) override = 0;
+	virtual ULONG AddRef() override = 0;
+	virtual ULONG Release() override = 0;
+};
